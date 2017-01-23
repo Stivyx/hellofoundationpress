30f6e6208a9302075c859f286ee0c7ee0620b8ea
Stivyx <stivyxx@gmail.com>
2017-01-23 13:00:48 +0100

    dff

--- wp-content/themes/HelloWordpress/header.php
+++ wp-content/themes/HelloWordpress/header.php
@@ -47,11 +47,11 @@
 			<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
 				<div class="top-bar-left">
 					<ul class="menu">
-						<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+						<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><i class="fa fa-home"></i></a></li>
+						<?php foundationpress_top_bar_r(); ?>
 					</ul>
 				</div>
 				<div class="top-bar-right">
-					<?php foundationpress_top_bar_r(); ?>
 
 					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
